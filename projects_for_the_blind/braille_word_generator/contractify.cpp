e60be890896f607690526afa136e03959fac9685
angstyloop <angstyloop@gmail.com>
2019-05-30 11:44:00 -0700

    commented contractify.cpp to provide clarity

--- projects_for_the_blind/braille_word_generator/contractify.cpp
+++ projects_for_the_blind/braille_word_generator/contractify.cpp
@@ -2,11 +2,14 @@
 //  a list of all the possible formations of the
 //  word using braille contractions.
 //
-//  Is a recursive solution possible?
+//  Is a recursive solution possible? --> Yes.
 
+// these are included in possibles.cpp. take these out after writing makefile.
 //#include<list>
 //#include<string>
 //#include<iostream>
+
+// take these out after writing makefile.
 #include<map>
 #include<utility>
 #include<vector>
@@ -19,6 +22,12 @@ using vl_t = std::list<std::vector<std::string>>;   //vector list
 using sl_t = std::list<std::string>;                //string list
 
 
+// Determine whether the smaller of two strings matches the beginning
+//  of the larger string. 
+//  e.g. match("abc","ab") --> true
+//       match("ab","abc") --> true  (order doesnt matter)
+//       match("abc","b")  --> false
+
 bool match(s_t s1, s_t s2) {
 
     //figure out which string is smaller
@@ -30,6 +39,7 @@ bool match(s_t s1, s_t s2) {
         big_pt = &s2;
         small_pt = &s1;
     }
+
     // use references for clarity
     const s_t& big = *big_pt;
     const s_t& small = *small_pt;
@@ -42,41 +52,60 @@ bool match(s_t s1, s_t s2) {
         if (small[i] != big[i])
             return false;
     }
+
     //otherwise the match is good
     return true;
 }
 
-
+// recursive helper function for contractify(). starts at the head of
+//  the string. uses current character to get a list of contractions
+//  that might start there. if any contraction matches starting at that
+//  character a recursive call to contractify is made, using the substring
+//  formed by removing the matching contraction from the head of the string.
+//  A memo, in the form of a vector of strings (contractions), is used
+//  to store the partitioning so far. A queue is passed along for
+//  storing the memos whenever the string argument to helper is empty.
 void helper( vl_t & Q, v_t memo, s_t str ) {
-    // list to store possible contractions
+
+    // a list to store possible contractions
     sl_t list;
 
-    if (str == "") {
-        //std::cout << "Adding " + memo + "to the queue." 
-        //          << std::endl;
-
+    //base case. if input string is empty, then memo must be a full
+    //  possible partitioning of str. stick it in the queue.
+    if (str == "") 
         Q.push_back(memo);
-    }
 
     else {
-        //std::cout << "Current character: " << str[0] << '\n';
+        // get a list of possible contractions
         list = possibles(str[0]);
-        for ( s_t x : list)
+
+        // loop through the contractions in list
+        for ( s_t contr : list)
+
             // if the contraction x fails to match
             //  or is too long for what's left of
             //  the string, helper isn't called.
-            if (x.size() <= str.size() && match(x, str)) {
-                //std::cout<< x << " does match."  <<std::endl;
-                memo.push_back(x);
+            if (contr.size() <= str.size() && match(contr, str)) {
+
+                // push the contraction contr into the string vector
+                //  memo representing the partitioned word so far.
+                memo.push_back(contr);
+
+                // call helper recursively with the new memo,
+                //  and the substring formed by removing contr
+                //  from the beginning of the word string str.
                 helper( Q
                 ,   memo
-                ,   str.substr(x.size(), str.size()-x.size())
+                ,   str.substr(contr.size(), str.size()-contr.size())
                 );
             }
 
     }
 }
 
+// given a word, return a list of vectors of strings corresponding
+//  to all the possible partitionings of the word using the single-
+//  character contractions listed in possibles.cpp.
 vl_t contractify ( s_t word ){
     vl_t Q;
     v_t memo;
@@ -84,6 +113,7 @@ vl_t contractify ( s_t word ){
     return Q; 
 }
 
+// a test program for contractify(). does it work? --> yes!
 int main() {
     sl_t str_list = {
         "vegetable"
